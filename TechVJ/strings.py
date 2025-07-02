fed32427c33a40c1f79c4d2ff33b6e0e92ea3ea6
akash2316y <iamakashrabha@gmail.com>
2025-07-02 21:03:43 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 HELP_TXT = """**🌟 Help Menu** 
 
 **__FOR PRIVATE CHATS__**
@@ -30,7 +26,3 @@ https://t.me/c/xxxx/101 - 120
 ```
 
 __note that space in between doesn't matter__"""
-       
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
