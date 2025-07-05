f1f1aa5dce31dfbfe57ba453363e08ba63e8f22c
akash2316y <iamakashrabha@gmail.com>
2025-07-05 11:47:34 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,28 +2,27 @@ import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
 class Database:
+    
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
-        self.channels = set()  # Channel management yahin shift kar diya
 
-    # ---------------- Users ----------------
     def new_user(self, id, name):
         return dict(
-            id=id,
-            name=name,
-            session=None,
+            id = id,
+            name = name,
+            session = None,
         )
-
+    
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
-
+    
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
+        user = await self.col.find_one({'id':int(id)})
         return bool(user)
-
+    
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
@@ -41,13 +40,4 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return user.get('session')
 
-    # ---------------- Channels ----------------
-    async def addchannel(self, channel_id):
-        self.channels.add(str(channel_id))
-
-    async def delchannel(self, channel_id):
-        self.channels.discard(str(channel_id))
-
-
-# 👇 Single unified db object
 db = Database(DB_URI, DB_NAME)
