15ba3af3a7b738c582103d7f7cd56781b51f3643
akash2316y <iamakashrabha@gmail.com>
2025-07-05 23:42:59 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -10,19 +10,21 @@ class Database:
 
     def new_user(self, id, name):
         return dict(
-            id = id,
-            name = name,
-            session = None,
+            id=id,
+            name=name,
+            session=None,
+            channel=None  # Add 'channel' field to store user's channel
         )
-    
+
     async def add_user(self, id, name):
-        user = self.new_user(id, name)
-        await self.col.insert_one(user)
-    
+        if not await self.is_user_exist(id):
+            user = self.new_user(id, name)
+            await self.col.insert_one(user)
+
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.col.find_one({'id': int(id)})
         return bool(user)
-    
+
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
@@ -38,6 +40,18 @@ class Database:
 
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('session')
+        return user.get('session') if user else None
 
+    async def add_channel(self, user_id, channel_id):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'channel': channel_id}}, upsert=True)
+
+    async def del_channel(self, user_id):
+        await self.col.update_one({'id': int(user_id)}, {'$unset': {'channel': ""}})
+
+    async def get_channel(self, user_id):
+        user = await self.col.find_one({'id': int(user_id)})
+        return user.get('channel') if user and 'channel' in user else None
+
+
+# Initialize Database
 db = Database(DB_URI, DB_NAME)
